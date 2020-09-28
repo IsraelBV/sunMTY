5ca1bc9bf18dd0606580206d1970eaf91a4caf96
Israel <israelbalamv@gmail.com>
2020-09-28 14:09:12 -0500

    Cambios septiembre namespaces
    se quito un schemalocation sin uso

--- Nominas/Nomina.Procesador/TimbradoCoreV2.cs
+++ Nominas/Nomina.Procesador/TimbradoCoreV2.cs
@@ -617,7 +617,7 @@ XslCompiledTransform transformador,
 
 
                 comprobante33.LugarExpedicion = emisorDatos.CP; //codigo postal del cat del sat  [0-9]{5}
-                comprobante33.schemaLocation = "http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv33.xsd http://www.sat.gob.mx/nomina12 http://www.sat.gob.mx/sitio_internet/cfd/nomina/nomina12.xsd http://www.sat.gob.mx/informacion_fiscal/factura_electronica/Documents/Complementoscfdi/nomina12.xsd";
+                comprobante33.schemaLocation = "http://www.sat.gob.mx/cfd/3 http://www.sat.gob.mx/sitio_internet/cfd/3/cfdv33.xsd http://www.sat.gob.mx/nomina12 http://www.sat.gob.mx/sitio_internet/cfd/nomina/nomina12.xsd";// http://www.sat.gob.mx/informacion_fiscal/factura_electronica/Documents/Complementoscfdi/nomina12.xsd";
                 //comprobante33.Confirmacion = "";
 
                 comprobante33.SubTotal = subtotal;
